2f249a7cd49719ddae4f1292cab90aa341878be6
Christopher Everett <cce29555@gmail.com>
2019-08-26 21:07:07 -0400

    Playing around with basic go functions

---
+++ test.go
@@ -0,0 +1,18 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	var a,b,c, ctoo, c2, x int = 1, 1, 1, 1, 1, 1
+	var sum = a + b + c + ctoo+ c2
+	var test = [4]int{1, 2, 3, 4}
+	
+	for x = 0; x < 4; x++{
+	fmt.Println(test[x])	
+	}
+
+  fmt.Println("Hello, 世界")
+	fmt.Println(sum)
+	
+}
